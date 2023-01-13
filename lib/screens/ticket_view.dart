e0a9f1590e0a227b5bafd6dc8506db220e5fa2e8
Abinnnn <mr.adhityabayu@gmail.com>
2023-01-13 23:58:27 +0700

    adding ticket display

---
+++ lib/screens/ticket_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:ticketing_app/utils/app_layout.dart';
+
+class TicketView extends StatelessWidget {
+  const TicketView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
+    return SizedBox(
+      width: size.width,
+      height: 200,
+      child: Container(
+        child: Column(children: []),
+      ),
+    );
+  }
+}
