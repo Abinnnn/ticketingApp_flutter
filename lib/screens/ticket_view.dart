a34319f870af6f7ff384976f4c4225e0d73112ab
Abinnnn <mr.adhityabayu@gmail.com>
2023-01-15 21:32:34 +0700

    ticketing view style

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:ticketing_app/utils/app_layout.dart';
+import 'package:ticketing_app/widgets/thick_container.dart';
+
+import '../utils/app_styles.dart';
 
 class TicketView extends StatelessWidget {
   const TicketView({Key? key}) : super(key: key);
@@ -13,7 +17,50 @@ class TicketView extends StatelessWidget {
       width: size.width,
       height: 200,
       child: Container(
-        child: Column(children: []),
+        margin: const EdgeInsets.only(left: 16),
+        child: Column(children: [
+          Container(
+              decoration: BoxDecoration(
+                color: Color(0xff526799),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(21),
+                  topRight: Radius.circular(21),
+                ),
+              ),
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text("NYC",
+                          style: Styles.headLineStyle3.copyWith(
+                            color: Colors.white,
+                          )),
+                      Expanded(child: Container()),
+                      ThickContainer(),
+                      Expanded(
+                          child: SizedBox(
+                        height: 24,
+                        child: LayoutBuilder(
+                          builder: (BuildContext context,
+                              BoxConstraints constraints) {
+                            return Flex(
+                                direction: Axis.horizontal,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children:
+                                    List.generate(5, (index) => Text("----")));
+                          },
+                        ),
+                      )),
+                      ThickContainer(),
+                      const Spacer(),
+                      Text("London", style: Styles.headLineStyle3),
+                    ],
+                  ),
+                ],
+              )),
+        ]),
       ),
     );
   }
