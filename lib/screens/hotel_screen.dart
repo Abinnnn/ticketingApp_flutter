6339d693bdc7515201960b3cc392e393a361cbdd
Abinnnn <mr.adhityabayu@gmail.com>
2023-01-17 21:35:34 +0700

    detailed hotel view

---
+++ lib/screens/hotel_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:gap/gap.dart';
+import 'package:ticketing_app/utils/app_layout.dart';
+
+import '../utils/app_styles.dart';
+
+class HotelScreen extends StatelessWidget {
+  const HotelScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
+    return Container(
+      width: size.width * 0.6,
+      height: 350,
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 17),
+      margin: EdgeInsets.only(right: 17, top: 5),
+      decoration: BoxDecoration(
+        color: Styles.primaryColor,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade200,
+            blurRadius: 10,
+            spreadRadius: 10,
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 180,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Styles.primaryColor,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  "assets/images/one.png",
+                ),
+              ),
+            ),
+          ),
+          const Gap(10),
+        ],
+      ),
+    );
+  }
+}
