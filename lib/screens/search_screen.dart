2563a96429d17f04e2247b28e2bfbe041ad857d1
Abinnnn <mr.adhityabayu@gmail.com>
2023-01-18 21:02:38 +0700

    adding search screen

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:gap/gap.dart';
+import 'package:ticketing_app/utils/app_layout.dart';
+
+import '../utils/app_styles.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: [
+          Gap(AppLayout.getHeight(40)),
+          Text("Search", style: Styles.headLineStyle1.copyWith(fontSize: 35)),
+        ],
+      ),
+    );
+  }
+}
