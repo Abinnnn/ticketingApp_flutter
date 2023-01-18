bdbe1bbdfca794078fdc712f692cefda5e510c49
Abinnnn <mr.adhityabayu@gmail.com>
2023-01-18 20:59:20 +0700

    layout calc

--- lib/utils/app_layout.dart
+++ lib/utils/app_layout.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
 
 class AppLayout {
   static getSize(BuildContext context) => MediaQuery.of(context).size;
+
+  static getScreenheight() {
+    return Get.height;
+  }
+
+  static getScreenwidth() {
+    return Get.width;
+  }
+
+  static getHeight(double pixels) {
+    double x = getScreenheight() / pixels;
+    return getScreenheight() / x;
+  }
+
+  static getWidth(double pixels) {
+    double x = getScreenwidth() / pixels;
+    return getScreenwidth() / x;
+  }
 }
