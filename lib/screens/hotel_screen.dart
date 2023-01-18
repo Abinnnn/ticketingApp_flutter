0813c6091e8b2149b49f2b22e9dbbe1a7eb740ed
Abinnnn <mr.adhityabayu@gmail.com>
2023-01-18 20:52:01 +0700

    change the text value from app_info_list.dart

--- lib/screens/hotel_screen.dart
+++ lib/screens/hotel_screen.dart
@@ -8,14 +8,16 @@ import 'package:ticketing_app/utils/app_layout.dart';
 import '../utils/app_styles.dart';
 
 class HotelScreen extends StatelessWidget {
-  const HotelScreen({Key? key}) : super(key: key);
+  final Map<String, dynamic> hotel;
+  const HotelScreen({Key? key, required this.hotel}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    print('Hotel price is ${hotel['price']}');
     final size = AppLayout.getSize(context);
     return Container(
       width: size.width * 0.6,
-      height: 350,
+      height: AppLayout.getHeight(350),
       padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 17),
       margin: EdgeInsets.only(right: 17, top: 5),
       decoration: BoxDecoration(
@@ -33,19 +35,33 @@ class HotelScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            height: 180,
+            height: AppLayout.getHeight(180),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
+              borderRadius: BorderRadius.circular(AppLayout.getHeight(7)),
               color: Styles.primaryColor,
               image: DecorationImage(
                 fit: BoxFit.cover,
                 image: AssetImage(
-                  "assets/images/one.png",
+                  "assets/images/${hotel['image']}",
                 ),
               ),
             ),
           ),
           const Gap(10),
+          Text(
+            "${hotel['place']}",
+            style: Styles.headLineStyle2.copyWith(color: Styles.kakiColor),
+          ),
+          const Gap(5),
+          Text(
+            "${hotel['destination']}",
+            style: Styles.headLineStyle3.copyWith(color: Colors.white),
+          ),
+          const Gap(8),
+          Text(
+            '\$${hotel['price']}/night',
+            style: Styles.headLineStyle1.copyWith(color: Styles.kakiColor),
+          )
         ],
       ),
     );
