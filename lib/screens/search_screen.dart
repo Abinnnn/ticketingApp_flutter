4599f5673b7533d6303471521673919454514da8
Abinnnn <mr.adhityabayu@gmail.com>
2023-01-19 17:28:19 +0700

    add bottom section of search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -4,20 +4,190 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:gap/gap.dart';
 import 'package:ticketing_app/utils/app_layout.dart';
+import 'package:ticketing_app/widgets/icon_text_widget.dart';
 
 import '../utils/app_styles.dart';
+import '../widgets/double_text_widget.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
     return Scaffold(
       backgroundColor: Styles.bgColor,
       body: ListView(
+        padding: EdgeInsets.symmetric(
+            horizontal: AppLayout.getWidth(20),
+            vertical: AppLayout.getHeight(20)),
         children: [
+          Gap(AppLayout.getHeight(5)),
+          Text("What are\nyou looking for ?",
+              style: Styles.headLineStyle1.copyWith(
+                  fontSize: AppLayout.getWidth(30),
+                  fontWeight: FontWeight.w700)),
+          Gap(AppLayout.getHeight(20)),
+          FittedBox(
+            child: Container(
+              padding: EdgeInsets.all(3.5),
+              child: Row(
+                children: [
+                  //airline ticket section
+                  Container(
+                    width: size.width * .44,
+                    padding:
+                        EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.horizontal(
+                          left: Radius.circular(AppLayout.getHeight(50))),
+                      color: Colors.white,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Airline ticket",
+                      ),
+                    ),
+                  ),
+
+                  //hotels section
+
+                  Container(
+                    width: size.width * .44,
+                    padding:
+                        EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.horizontal(
+                          right: Radius.circular(AppLayout.getHeight(50))),
+                      color: Colors.transparent,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Hotels",
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(AppLayout.getHeight(50)),
+                  color: const Color(0xFFF4F6FD)),
+            ),
+          ),
+          Gap(AppLayout.getHeight(25)),
+          const AppIconText(
+              icon: Icons.flight_takeoff_rounded, text: "departure"),
+          Gap(AppLayout.getHeight(10)),
+          const AppIconText(icon: Icons.flight_land_rounded, text: "Arrival"),
+          Gap(AppLayout.getHeight(20)),
+          Container(
+            padding: EdgeInsets.symmetric(
+                vertical: AppLayout.getWidth(12),
+                horizontal: AppLayout.getWidth(12)),
+            decoration: BoxDecoration(
+                color: Color(0xD91130CE),
+                borderRadius: BorderRadius.circular(AppLayout.getWidth(5))),
+            child: Center(
+              child: Text("Find Tickets",
+                  style: Styles.textStyle.copyWith(color: Colors.white)),
+            ),
+          ),
           Gap(AppLayout.getHeight(40)),
-          Text("Search", style: Styles.headLineStyle1.copyWith(fontSize: 35)),
+          AppDoubleTextWidget(bigText: "Upcoming", smallText: "View All"),
+          Gap(AppLayout.getHeight(15)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: AppLayout.getHeight(400),
+                width: size.width * 0.42,
+                padding: EdgeInsets.symmetric(
+                    horizontal: AppLayout.getHeight(15),
+                    vertical: AppLayout.getWidth(15)),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.circular(AppLayout.getHeight(10)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade200,
+                        blurRadius: 1,
+                      ),
+                    ]),
+                child: Column(
+                  children: [
+                    Container(
+                      height: AppLayout.getHeight(190),
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.circular(AppLayout.getHeight(10)),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/images/sit.jpg"),
+                        ),
+                      ),
+                    ),
+                    Gap(AppLayout.getHeight(10)),
+                    Text(
+                      "20% discount on the early booking of this flight. Don't miss",
+                      style: Styles.headLineStyle2,
+                    )
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: size.width * 0.44,
+                        height: AppLayout.getHeight(210),
+                        decoration: BoxDecoration(
+                          color: Color(0xFF3Ab8b8),
+                          borderRadius:
+                              BorderRadius.circular(AppLayout.getHeight(18)),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                            horizontal: AppLayout.getHeight(15),
+                            vertical: AppLayout.getWidth(15)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Discount\nfor survey",
+                                style: Styles.headLineStyle2.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white)),
+                            Gap(AppLayout.getHeight(10)),
+                            Text(
+                                "Take the survey about our services and get discount",
+                                style: Styles.headLineStyle2.copyWith(
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                )),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        right: -45,
+                        top: -40,
+                        child: Container(
+                          padding: EdgeInsets.all(AppLayout.getHeight(30)),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                width: 18,
+                                color: Color(0xFF189999),
+                              ),
+                              color: Colors.transparent),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
     );
