882b006c2bdf4298a0aeb7a89a682bed63836e93
Abinnnn <mr.adhityabayu@gmail.com>
2023-01-22 23:54:57 +0700

    update ticket screen

--- lib/screens/ticket_screen.dart
+++ lib/screens/ticket_screen.dart
@@ -9,6 +9,8 @@ import 'package:ticketing_app/widgets/ticket_tabs.dart';
 import '../utils/app_info_list.dart';
 import '../utils/app_layout.dart';
 import '../utils/app_styles.dart';
+import '../widgets/layout_builder_widget.dart';
+import 'package:barcode_widget/barcode_widget.dart';
 
 class TicketScreen extends StatelessWidget {
   const TicketScreen({Key? key}) : super(key: key);
@@ -40,11 +42,14 @@ class TicketScreen extends StatelessWidget {
                   isColor: true,
                 ),
               ),
+              SizedBox(
+                height: 1,
+              ),
               Container(
                 decoration: BoxDecoration(
                   color: Colors.white,
                 ),
-                padding: EdgeInsets.symmetric(horizontal: 15),
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
                 margin:
                     EdgeInsets.symmetric(horizontal: AppLayout.getWidth(15)),
                 child: Column(
@@ -67,7 +72,105 @@ class TicketScreen extends StatelessWidget {
                         // AppColumnLayout right section
                       ],
                     ),
+                    Gap(AppLayout.getHeight(20)),
+                    AppLayoutBuilderWidget(
+                        sections: 15, isColor: false, width: 5),
+
+                    //
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        AppColumnLayout(
+                            firstText: "3578 232",
+                            secondText: "Number of E-Ticket",
+                            alignment: CrossAxisAlignment.start,
+                            isColor: false),
+
+                        AppColumnLayout(
+                          firstText: "b2SG28",
+                          secondText: "Booking Code",
+                          alignment: CrossAxisAlignment.end,
+                          isColor: false,
+                        )
+                        // AppColumnLayout right section
+                      ],
+                    ),
+
+                    //
+                    Gap(AppLayout.getHeight(20)),
+                    AppLayoutBuilderWidget(
+                        sections: 15, isColor: false, width: 5),
+                    Gap(AppLayout.getHeight(20)),
+                    //
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset(
+                                  "assets/images/visa.png",
+                                  scale: 10,
+                                ),
+                                Text(
+                                  " *** 2462",
+                                  style: Styles.headLineStyle3,
+                                )
+                              ],
+                            ),
+                            Gap(5),
+                            Text("Payment method",
+                                style: Styles.headLineStyle4),
+                          ],
+                        ),
+                        AppColumnLayout(
+                            firstText: "\$249,99",
+                            secondText: "Price",
+                            alignment: CrossAxisAlignment.end,
+                            isColor: false),
+                      ],
+                    ),
+
+                    //
+                    SizedBox(
+                      height: 1,
+                    ),
                   ],
+                ),
+              ),
+
+              //bar code
+              SizedBox(
+                height: 1,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10))),
+                margin: EdgeInsets.only(
+                  left: 15,
+                  right: 15,
+                ),
+                padding: EdgeInsets.only(top: 15, bottom: 15),
+                child: Container(
+                  color: Colors.white,
+                  padding:
+                      EdgeInsets.symmetric(horizontal: AppLayout.getHeight(15)),
+                  child: ClipRRect(
+                    borderRadius:
+                        BorderRadius.circular(AppLayout.getHeight(15)),
+                    child: BarcodeWidget(
+                      barcode: Barcode.code128(),
+                      data: 'https://github.com//Abinnnn',
+                      drawText: false,
+                      color: Styles.textColor,
+                      width: double.infinity,
+                      height: 70,
+                    ),
+                  ),
                 ),
               ),
             ],
