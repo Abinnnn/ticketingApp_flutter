e49c23efc21b18e30d16a5222caae2afb26d6a50
Abinnnn <mr.adhityabayu@gmail.com>
2023-01-22 23:56:08 +0700

    minor update

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -10,15 +10,13 @@ class Styles {
   static Color kakiColor = const Color(0xFFd2bdb6);
 
   static TextStyle textStyle =
-      TextStyle(fontSize: 16, color: textColor, fontWeight: FontWeight.w500);
+      TextStyle(fontSize: 26, color: textColor, fontWeight: FontWeight.w500);
   static TextStyle headLineStyle1 =
-      TextStyle(fontSize: 26, color: textColor, fontWeight: FontWeight.bold);
+      TextStyle(fontSize: 21, color: textColor, fontWeight: FontWeight.bold);
   static TextStyle headLineStyle2 =
       TextStyle(fontSize: 17, color: textColor, fontWeight: FontWeight.bold);
-  static TextStyle headLineStyle3 = TextStyle(
-      fontSize: 12,
-      color: Color.fromARGB(255, 147, 147, 147),
-      fontWeight: FontWeight.w500);
+  static TextStyle headLineStyle3 =
+      TextStyle(fontSize: 12, fontWeight: FontWeight.w600);
   static TextStyle headLineStyle4 = TextStyle(
       fontSize: 14, color: Colors.grey.shade500, fontWeight: FontWeight.w500);
 }
